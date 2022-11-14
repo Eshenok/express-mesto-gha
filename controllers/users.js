9f8973a1bf07d111ad40b865e1352427ee6e7c82
Alexandr <102781985+Eshenok@users.noreply.github.com>
2022-11-14 16:23:08 +0300

    make PATCH

--- controllers/users.js
+++ controllers/users.js
@@ -22,15 +22,15 @@ module.exports.createUser = (req, res) => {
 }
 
 module.exports.updateUser = (req, res) => {
-	const {name, about} = req.params;
-	User.findByIdAndUpdate(req.user._id, {name, about})
+	const {name, about} = req.body;
+	User.findByIdAndUpdate(req.user._id, {name, about}, {new: true})
 		.then(user => res.send({data: user}))
 		.catch(err => res.status(500).send({message: err.message}))
 }
 
 module.exports.updateUserAvatar = (req, res) => {
 	const {avatar} = req.body;
-	User.findByIdAndUpdate(req.user._id, {avatar})
+	User.findByIdAndUpdate(req.user._id, {avatar}, {new: true})
 		.then(user => res.send({data: user}))
 		.catch(err => res.status(500).send({message: err.message}))
 }
