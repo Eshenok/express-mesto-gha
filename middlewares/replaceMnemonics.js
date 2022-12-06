0eb6533f172edb895c98392e1bcacfc9c2362fce
Alexandr <102781985+Eshenok@users.noreply.github.com>
2022-12-06 17:25:03 +0300

    lint

--- middlewares/replaceMnemonics.js
+++ middlewares/replaceMnemonics.js
@@ -2,13 +2,14 @@ const escape = require('escape-html');
 
 module.exports.replaceMnemonics = (req, res, next) => {
   for (const key in req.body) {
-    if (key === 'password') {
-      continue;
+    if (key !== 'password') {
+      req.body[key] = escape(req.body[key]);
     }
-    req.body[key] = escape(req.body[key]);
   }
   for (const key in req.params) {
-    req.params[key] = escape(req.params[key]);
+    if (key === 'id' || key === 'cardId') {
+      req.params[key] = escape(req.params[key]);
+    }
   }
   next();
 };
