9989868b1ec71d3eb76e3a2350d5d57a19efb0f0
Alexandr <102781985+Eshenok@users.noreply.github.com>
2022-12-16 20:25:40 +0600

    cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,6 +1,6 @@
 module.exports = function() {
   return function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000, https://voloshin.eshenok.nomoredomains.club, http://voloshin.eshenok.nomoredomains.club");
+    res.header("Access-Control-Allow-Origin", "https://voloshin.eshenok.nomoredomains.club");
     res.header("Access-Control-Allow-Credentials", "true");
     res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, origin, Authorization");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
