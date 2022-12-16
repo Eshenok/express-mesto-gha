385b79517852524bba265139eb75f73240885b1b
Alexandr <102781985+Eshenok@users.noreply.github.com>
2022-12-16 20:33:19 +0600

    cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,10 +1,15 @@
+const allowedOrigin = ["https://voloshin.eshenok.nomoredomains.club", "localhost:3000"];
+// allowedOrigin.includes(origin)
 module.exports = function() {
   return function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://voloshin.eshenok.nomoredomains.club");
-    res.header("Access-Control-Allow-Credentials", "true");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, origin, Authorization");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
-    res.header("Access-Control-Max-Age", 86400);
-    res.end();
+    const { origin } = req.headers;
+    if (true) {
+      res.header("Access-Control-Allow-Origin", origin);
+      res.header("Access-Control-Allow-Credentials", "true");
+      res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, origin, Authorization");
+      res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
+      res.header("Access-Control-Max-Age", 86400);
+      next();
+    }
   };
 }
